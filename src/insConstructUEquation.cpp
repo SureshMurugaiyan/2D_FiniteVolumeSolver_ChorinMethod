877d35b4ce719bcbeb5a1481218a641c9c535d1b
Suresh Murugaiyan <sureshm@vt.edu>
2020-03-17 19:51:33 -0400

    re 400 validated with Ghia and Ghia results

--- src/insConstructUEquation.cpp
+++ src/insConstructUEquation.cpp
@@ -14,7 +14,7 @@ void insConstructUEquation(mesh_t *mesh){
 	double Ueqn=0.0;
 
 
-	double GammaLaplacian  	= -0.01;   // Constant Coefficient for viscous terms
+	double GammaLaplacian  	= -0.0025;   // Constant Coefficient for viscous terms Re = 100=> 0.01, Re = 400=>0.0025
 	double GammaDivergence 	=  1.0;   // Constant Coefficient for divergence terms
 
 
--- src/solverSetControls.cpp
+++ src/solverSetControls.cpp
@@ -3,10 +3,10 @@
 
 void solverSetControls(mesh_t *mesh){
 
-	mesh->timeStepMax		= 2000;
-	mesh->finalTime  		= 2000*0.01;
+	mesh->timeStepMax		= 10000;
+	mesh->finalTime  		= 10000*0.01;
 	mesh->deltatime  		= 0.01;
-	mesh->WriteFrequency	= 100;
+	mesh->WriteFrequency	= 1000;
 	mesh->nCorrMax  		= 1;
 	mesh->time  			= 0.0; // start time
 	mesh->timeStep  		= 0; // start time step
